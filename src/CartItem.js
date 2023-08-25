a4bc37d631c913ab9a07eefe37bcb39e15a7280b
Subramanian1503 <roopan1503@gmail.com>
2023-08-25 19:18:18 +0530

    Added state and constructor for the Cart item component

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 class CartItem extends React.Component {
 
+    // Defining the constructor to maintain state for the component
+    constructor() {
+        super();
+        this.state = {
+            title: 'Phone',
+            price: 999,
+            qty: 4,
+            img: ''
+        }
+    }
+
     render() {
+        const { title, price, qty } = this.state;
+
         return (
             <div className='cart-items'>
                 <div className="left-block">
@@ -10,16 +23,19 @@ class CartItem extends React.Component {
                 </div>
                 <div className="right-block">
                     <div style={{ fontSize: 25 }}>
-                        Phone
+                        {title}
                     </div>
-                    <div style={{ color : '#777' }}>
-                        999
+                    <div style={{ color: '#777' }}>
+                        {price}
                     </div>
-                    <div style={{ color: '#777'}}>
-                        Qty: 4
+                    <div style={{ color: '#777' }}>
+                        Qty: {qty}
                     </div>
-                    <div>
+                    <div className='cart-item-action'>
                         {/* Buttons to alter quantity */}
+                        <img alt="Add" className='action-icons' src="https://cdn-icons-png.flaticon.com/128/992/992651.png" />
+                        <img alt="Remove" className='action-icons' src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" />
+                        <img alt="Delete" className='action-icons' src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" />
                     </div>
                 </div>
             </div>
