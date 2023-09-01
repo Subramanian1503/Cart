7debc8e7cb365346e4f4deba4fed44646333f0cd
Subramanian1503 <roopan1503@gmail.com>
2023-09-01 19:05:16 +0530

    Added Event handler to increase and decrease quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -8,10 +8,41 @@ class CartItem extends React.Component {
         this.state = {
             title: 'Phone',
             price: 999,
-            qty: 4,
+            qty: 1,
             img: ''
         }
     }
+
+    // Utility methods
+    // Here we defined a lambda function instead of normal function to bind the required object with it
+    increaseQuantity = () => {
+        // Get the current quantity
+        // const current_qty = this.state.qty;
+        // Increase the quantity value and re-render the JSX
+        // Set state will do shallow merge the given object with current state and re render the JSX
+        // form - 1 when no previous state required
+        // this.setState({
+        //     qty: current_qty + 1,
+        // });
+
+        // form - 2 when previous state required
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1,
+            }
+        })
+    }
+
+    decreaseQuantity = () => {
+        // Get the current quantity 
+        // Decrease the value and re render the JSX
+        this.setState((previousState) => {
+            return {
+                qty: previousState.qty - 1,
+            }
+        })
+    }
+
 
     render() {
         const { title, price, qty } = this.state;
@@ -33,9 +64,9 @@ class CartItem extends React.Component {
                     </div>
                     <div className='cart-item-action'>
                         {/* Buttons to alter quantity */}
-                        <img alt="Add" className='action-icons' src="https://cdn-icons-png.flaticon.com/128/992/992651.png" />
-                        <img alt="Remove" className='action-icons' src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" />
-                        <img alt="Delete" className='action-icons' src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" />
+                        <img alt="Add" className='action-icons' src="https://cdn-icons-png.flaticon.com/128/992/992651.png" onClick={this.increaseQuantity} />
+                        <img alt="Remove" className='action-icons' src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" onClick={this.decreaseQuantity}/>
+                        <img alt="Delete" className='action-icons' src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png" />
                     </div>
                 </div>
             </div>
