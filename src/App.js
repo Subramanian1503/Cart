533724cf465534fd9663a346096a02d021ddd66b
Subramanian1503 <roopan1503@gmail.com>
2023-09-15 19:06:24 +0530

    Persist data using firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react'
 import Cart from './Cart'
 import NavBar from './NavBar'
+import db from './index'
 
 class App extends React.Component {
 
@@ -9,28 +10,29 @@ class App extends React.Component {
     super();
     this.state = {
       products: [
-        {
-          id: 1,
-          name: 'Iphone 13',
-          qty: 2,
-          price: 65000,
-          img: 'https://www.myg.in/images/thumbnails/624/460/detailed/21/APPLE_iPhone_13-2.jpeg'
-        },
-        {
-          id: 2,
-          name: 'Iphone 14',
-          qty: 1,
-          price: 90000,
-          img: 'https://assets.sangeethamobiles.com/product_img/6974/1663134480_JEm.jpg'
-        },
-        {
-          id: 3,
-          name: 'Iphone 14 pro max',
-          qty: 2,
-          price: 172000,
-          img: 'https://storage.yandexcloud.net/the-istore.ru-backup/iblock/4e2/4e239fbb9015580fe2f81c70b6417422/a9aae5d7b6cc927f8728fa6a1ab18a30.jpeg'
-        }
-      ]
+        // {
+        //   id: 1,
+        //   name: 'Iphone 13',
+        //   qty: 2,
+        //   price: 65000,
+        //   img: 'https://www.myg.in/images/thumbnails/624/460/detailed/21/APPLE_iPhone_13-2.jpeg'
+        // },
+        // {
+        //   id: 2,
+        //   name: 'Iphone 14',
+        //   qty: 1,
+        //   price: 90000,
+        //   img: 'https://assets.sangeethamobiles.com/product_img/6974/1663134480_JEm.jpg'
+        // },
+        // {
+        //   id: 3,
+        //   name: 'Iphone 14 pro max',
+        //   qty: 2,
+        //   price: 172000,
+        //   img: 'https://storage.yandexcloud.net/the-istore.ru-backup/iblock/4e2/4e239fbb9015580fe2f81c70b6417422/a9aae5d7b6cc927f8728fa6a1ab18a30.jpeg'
+        // }
+      ],
+      loading: true
     }
   }
 
@@ -96,21 +98,95 @@ class App extends React.Component {
     return count;
   }
 
+  getTotalAmount = () => {
+
+    const { products } = this.state;
+
+    let totalAmount = 0;
+
+    products.forEach((product) => {
+      totalAmount += (product.qty * product.price);
+    })
+
+    return totalAmount;
+  }
+
+  componentDidMount() {
+    // Get all the data from firebase
+    // db.collection("products").get().then((snapshots) => {
+
+    //   // Getting the products from snapshot
+    //   const products = snapshots.docs.map((doc) => {
+    //     console.log(doc);
+    //     const data = doc.data();
+    //     data["id"] = doc.id;
+    //     return data;
+    //   });
+
+    //   // Set the products as state of the component
+    //   this.setState({
+    //     products,
+    //     loading: false,
+    //   })
+    // })
+
+    // Get all data from DB whenever there is a update in data of DB
+    db.collection("products").onSnapshot((snapshots) => {
+
+      // Getting the products from snapshot
+      const products = snapshots.docs.map((doc) => {
+        console.log(doc);
+        const data = doc.data();
+        data["id"] = doc.id;
+        return data;
+      });
+
+      // Set the products as state of the component
+      this.setState({
+        products,
+        loading: false,
+      })
+    })
+
+  }
+
+  addProduct = () => {
+
+    //Get the collection from firestore to add products
+    db.collection("products").add({
+      img: "https://www.91-img.com/pictures/148162-v4-apple-iphone-15-pro-max-mobile-phone-large-10.jpg",
+      name: "Iphone 15 pro max",
+      price: 175000,
+      qty: 1
+    }).then((addedDocument) => {
+      console.log("Product added successfully", addedDocument);
+    }).catch((error) => {
+      console.log("Error occured while trying to add products", error);
+    })
+
+  }
+
   render() {
 
     // Extarct required informations to populate in JSX
-    const { products } = this.state;
+    const { products, loading } = this.state;
     const cartCount = this.getCartCount();
+    const totalAmount = this.getTotalAmount();
 
     // Define JSX with extracted information from above
     return (
       <div className="App">
         <NavBar cartCount={cartCount} />
+        <img onClick={this.addProduct} style={{height: '50px', width: '50px', marginLeft: '1450px', cursor:'pointer', borderRadius: '50%'}} src="https://w7.pngwing.com/pngs/68/239/png-transparent-number-computer-icons-plus-miscellaneous-game-plus.png"></img>
+        {loading && <h1>Loading products..</h1>}
         <Cart
           products={products}
           increaseQuantity={this.handleIncreaseQuantity}
           decreaseQuantity={this.handleDecreaseQuantity}
           deleteProduct={this.handleDeleteProduct} />
+        <div style={{ fontSize: 20, padding: 20 }}>
+          TOTAL: {totalAmount}
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCviuhdFDuc0t8nWMO3ULpux7zcZKDnodk",
+  authDomain: "cart-web-app-e6124.firebaseapp.com",
+  projectId: "cart-web-app-e6124",
+  storageBucket: "cart-web-app-e6124.appspot.com",
+  messagingSenderId: "240486273102",
+  appId: "1:240486273102:web:cacf27895a01cb9cb9b31f"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = app.firestore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+export default db;
